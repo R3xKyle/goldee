f3be6e41d7a04adbeec983fc516cbbf77d7dcd98
zuoyuanh <david@collegemate.me>
2018-02-04 10:53:31 -0800

    updated new post

--- goldee/frontEndFiles/src/router.js
+++ goldee/frontEndFiles/src/router.js
@@ -7,16 +7,19 @@ import Home from './components/Home'
 import Register from './components/Register'
 import Login from './components/Login'
 import Profile from './components/Profile'
-import FeedWall from './components/FeedWall'
-import Post from './components/Post'
+import NewPost from './components/NewPost/NewPost'
+import NewPostCategory from './components/NewPost/NewPostCategory'
+import NewPostContactInfo from './components/NewPost/NewPostContactInfo'
+
 
 const routes = [
   { path: '/', component: Home },
   { path: '/register', component: Register },
   { path: '/login', component: Login},
   { path: '/profile', component: Profile},
-  { path: '/feedwall', component: FeedWall},
-  { path: '/post', component: Post }
+  { path: '/newpost/post', component: NewPost },
+  { path: '/newpost/contact', component: NewPostContactInfo },
+  { path: '/newpost', component: NewPostCategory }
 ]
 
 const router = new VueRouter({
