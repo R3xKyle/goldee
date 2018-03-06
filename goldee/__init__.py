bae21e5e23473f58338f79cd50b6bfb837d9634f
R3xKyle <kyle@ringler.com>
2018-03-05 17:05:51 -0800

    Started refactor

--- goldee/__init__.py
+++ goldee/__init__.py
@@ -1,1 +1,27 @@
-print("Hello")
+from flask import Flask
+
+from config import app_create
+
+application = app_create(__name__)
+db = SQLAlchemy(application)
+
+from goldee.sweep import DatabaseSweep
+#DatabaseSweep()
+
+from goldee import models, router
+
+
+@application.teardown_request
+def teardown_request(exception):
+	if exception:
+		db.session.rollback()
+		db.session.remove()
+	db.session.remove()
+
+# Need to close the session after each request or application context shutdown
+@application.teardown_appcontext
+def shutdown_session(exception=None):
+    if exception:
+        db.session.rollback()
+	db.session.remove()
+
--- goldee/views/index.py
+++ goldee/views/index.py
@@ -1,31 +1,22 @@
 from flask import Blueprint, render_template, request
+
 from goldee.database import insertSimple, getUser, getFeed
 
+IndexBP = Blueprint('index', __name__, template_folder = "../frontEndFiles/dist")
 
-IndexBP = Blueprint('index', __name__, template_folder = "../frontEndFiles")
-
-
-from goldee.models import SimpleUser
-from goldee.database import insertSimple
-from goldee.forms import SimpleUserForm
-
-'''
-@IndexBP.route('/', methods = ['GET', 'POST'])
+@IndexBP.route('/', methods = ['GET'])
 def index():
-	zip = 93410
-	return render_template("index.html", posts = getFeed(zip, 20))
-'''
+    return render_template('index.html')
 
-@IndexBP.route('/', methods = ['GET', 'POST'])
-def index():
-    form = SimpleUserForm()
-    if form.validate_on_submit():
-        user = SimpleUser()
-        user.Name = form.name.data
-        user.Email = form.email.data
-        insertSimple(user)
-    return render_template('index.html', form = form)
-
-
-
-
+@IndexBP.route('/feed',  methods = ['GET'])
+def feed():
+    page = requests.args.get('page', 1, type=int)
+    try:
+        posts = getFeed(page)
+        next_url = url_for('feed', page = posts.next_num) if posts.has_next else None
+        prev_url = url_for('feed', page = posts.prev_num) if posts.has_prev else None
+    except:
+        flash("We're sorry, something went wrong.")
+        return redirect('/')
+    return render_template('feedpage.html', posts = posts.items, 
+                            next_url = next_url, prev_url = prev_url)
